48ff32738266c9301e30c286da7136fc7fb3f784
XenoProxy <44860152+XenoProxy@users.noreply.github.com>
2023-01-11 21:38:58 +0300

    [+] upload catalog

---
+++ admin/explorer.php
@@ -0,0 +1,18 @@
+<?php
+require_once '../elements/header.php'; 
+
+$url = $_SERVER['REQUEST_URI'];
+if ($url == '/admin/explorer.php') {
+    header('Location: /');
+}
+
+$path = getcwd();
+echo $path;
+
+//загрузка файлов на сервер
+$destPath = './uploads';
+if (!(file_exists($destPath))) {
+    mkdir('../uploads');
+}
+
+
