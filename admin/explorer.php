96a8611005d48593a855fe57b3ad02c2f6c63b68
XenoProxy <44860152+XenoProxy@users.noreply.github.com>
2023-01-13 14:40:07 +0300

    [*] Home page button

--- admin/explorer.php
+++ admin/explorer.php
@@ -1,15 +1,15 @@
 <?php
 require_once '../elements/header.php'; 
 
+// return to the home page
+echo "<a href='/?folder=/admin/..'>Home</a><br>";
+
 $url = $_SERVER['REQUEST_URI'];
 if ($url == '/admin/explorer.php') {
     header('Location: /');
 }
 
-$path = getcwd();
-echo $path;
-
-//загрузка файлов на сервер
+// upload files
 $destPath = '../uploads';
 if (!(file_exists($destPath))) {
     mkdir('../uploads');
